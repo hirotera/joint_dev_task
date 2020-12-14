a2a0f0b6de5c920bf72a50d7505267a39d9e6ede
hirotera <pomme-de-terre@hotmail.co.jp>
2020-12-14 22:48:21 +0900

    php課題17まで

--- task.php
+++ task.php
@@ -209,27 +209,47 @@ foreach($users as $key=>$user){
 
 echo PHP_EOL;
 
-// print("#####q17#####".PHP_EOL);
-// class User
+## Q17. 次の Userクラス 内にinfoメソッドを追加し，期待する出力結果になるようにして下さい。
+print("#####q17#####".PHP_EOL);
+class User
+{
+  protected $name;
+  protected $age;
+  protected $gender;
+
+  function __construct($user_name,$user_age,$user_gender) {
+    $this -> name = $user_name;
+    $this -> age = $user_age;
+    $this -> gender = $user_gender;
+  }
+  
+  function info(){
+    print("名前:".$this->name.PHP_EOL);
+    print("年齢:".$this->age.PHP_EOL);
+    print("性別:".$this->gender.PHP_EOL);
+  }
+}
+
+$user1 = new User("神里",32,"男");
+$user2 = new User("あじー",32,"男");
+
+$user1->info();
+print("-------------".PHP_EOL);
+$user2->info();
+
+echo PHP_EOL;
+
+// ## Q18. 年齢を用いた場合分けを利用して，期待する出力結果になるようなManクラスを作成して下さい。
+// print("#####q18#####".PHP_EOL);
+// // ```
+// // こんにちは，あじーと申します。宜しくお願いいたします。
+// // はいさいまいど〜，ゆたぼんです！！！
+// // ```
+//   # コードを追加
+// class Man
 // {
 
-//   # コードを追加
-
 // }
-
-// $user1 = new User("神里",32,"男");
-// $user2 = new User("あじー",32,"男");
-
-// $user1->info();
-// print("-------------".PHP_EOL);
-// $user2->info();
-
-// echo PHP_EOL;
-
-// print("#####q18#####".PHP_EOL);
-
-//   # コードを追加
-
 // $man1 = new Man("あじー",32);
 // $man2 = new Man("ゆたぼん",10);
 
