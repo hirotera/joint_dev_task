f71be03e8c5950979dfb2b9fbce764bc07cd3ff0
hirotera <pomme-de-terre@hotmail.co.jp>
2020-12-15 19:32:13 +0900

    php課題19まで

--- task.php
+++ task.php
@@ -269,22 +269,22 @@ $man2->introduce();
 
 echo PHP_EOL;
 
-// print("#####q19#####".PHP_EOL);
-// class Item{
-//   # 以下を修正して下さい
+print("#####q19#####".PHP_EOL);
+class Item{
+  # 以下を修正して下さい
 
-//   protected $name;
+  protected $book_name;
 
-//   function __construct($book_name){
-//     $this->name = $book_name;
-//   }
-// }
-//   # 以下は変更しないで下さい
+  function __construct($book_name){
+    $this->name = $book_name;
+  }
+}
+  # 以下は変更しないで下さい
 
-// $book = new Item("ゼロ秒思考");
-// print($book->name.PHP_EOL);
+$book = new Item("ゼロ秒思考");
+print($book->name.PHP_EOL);
 
-// echo PHP_EOL;
+echo PHP_EOL;
 
 // print("#####q20#####".PHP_EOL);
 // class Human
